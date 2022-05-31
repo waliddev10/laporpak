5d03b9cf99e0353c2282b95499ff3a2f32cb27a3
walidark <mohwalidas2@gmail.com>
2022-05-31 15:47:04 +0800

    update skpd font size

--- resources/views/pdf/laporan_bulanan/skpd.blade.php
+++ resources/views/pdf/laporan_bulanan/skpd.blade.php
@@ -7,7 +7,7 @@
     }
 
     body {
-        font-size: 11pt;
+        font-size: 10pt;
     }
 
     table td,
@@ -28,12 +28,13 @@
         <thead style="border-bottom: 6px solid black; border-bottom-style: double; padding: 0 0 0.2cm 0;">
             <tr>
                 <td style="text-align: center; padding: 0;"><img src="{{ asset('assets/img/logo-kaltim.png') }}"
-                        height="110" />
+                        height="100" />
                 </td>
                 <td style="text-align: center; padding: 0;">
-                    <h2 style="margin: 0;">PEMERINTAH PROVINSI KALIMANTAN TIMUR</h2>
-                    <h1 style="margin: 0;">BADAN PENDAPATAN DAERAH</h1>
-                    <h3 style="font-size: 12pt; margin: 0;">UPTD. PELAYANAN PAJAK DAN RETRIBUSI DAERAH WILAYAH PPU</h3>
+                    <h2 style="margin: 0; font-size: 14pt;">PEMERINTAH PROVINSI KALIMANTAN TIMUR</h2>
+                    <h1 style="margin: 0; font-size: 18pt;">BADAN PENDAPATAN DAERAH</h1>
+                    <h3 style="margin: 0; font-size: 12pt;">UPTD. PELAYANAN PAJAK DAN RETRIBUSI DAERAH
+                        WILAYAH PPU</h3>
                     <h4 style="margin: 0;">Jalan Provinsi KM. 4,5 Penajam Telp. (0542) 7588802 Fax. (0542) 7200812</h4>
                     <h4 style="margin: 0;">P E N A J A M</h4>
                 </td>
@@ -41,13 +42,13 @@
         </thead>
     </table>
 
-    <div style="margin-top: 1.5cm; margin-bottom: 1cm; text-align: center;">
+    <div style="margin-top: 1.25cm; margin-bottom: 1cm; text-align: center;">
         <h3 style="margin: 0 0 0.2cm 0;">BERITA ACARA PENGGUNAAN SKKP</h3>
         <span>Nomor: 970/ /PPRD.PPU.02/{{
             \Terbilang::roman(\Carbon\Carbon::parse($tgl_ttd)->format('m')) }}/2022</span>
     </div>
 
-    <p style="text-indent: 1cm; text-align: justify; line-height: 16pt;">Pada hari ini {{
+    <p style="text-indent: 1cm; text-align: justify; line-height: 14pt;">Pada hari ini {{
         \Carbon\Carbon::parse($tgl_ttd)->dayName }} tanggal
         {{
         ucwords(\Terbilang::make(\Carbon\Carbon::parse($tgl_ttd)->format('d'))) }} Bulan {{
@@ -56,7 +57,7 @@
         bertanda tangan dibawah ini :
     </p>
 
-    <div style="margin-left: 2cm; line-height: 16pt;">
+    <div style="margin-left: 2cm; line-height: 14pt;">
         <table style="width: 100%; margin-top: 0; margin-bottom: 0;">
             <tr>
                 <td style="width: 0.5cm;">1.</td>
@@ -82,13 +83,13 @@
         </table>
     </div>
 
-    <p style="text-align: justify; line-height: 16pt;">Secara bersama-sama melakukan pemeriksaan
+    <p style="text-align: justify; line-height: 14pt;">Secara bersama-sama melakukan pemeriksaan
         pengeluaran/penggunaan Surat Ketetapan Kewajiban Pembayaran (SKKP) tanggal {{
         \Carbon\Carbon::parse($tgl_mulai)->isoFormat('D') }} s.d. {{
         \Carbon\Carbon::parse($tgl_selesai)->isoFormat('D MMMM Y') }}, dengan
         perincian sebagai berikut :</p>
 
-    <table class="table" style="font-size: 10pt; width: 100%; border-collapse: collapse; margin: 0 0 20pt 0;">
+    <table class="table" style="font-size: 9pt; width: 100%; border-collapse: collapse; margin: 0 0 20pt 0;">
         <tr style="border-bottom: 4px solid black; border-bottom-style: double;">
             <th style="border: 0.5pt solid black; width: 5%;">NO.</th>
             <th style="border: 0.5pt solid black; width: 25%;">TANGGAL</th>
@@ -133,14 +134,14 @@
         </tr>
     </table>
 
-    <p style="text-indent: 1cm; text-align: justify; line-height: 16pt;">Demikian Berita Acara ini dibuat dan
+    <p style="text-indent: 1cm; text-align: justify; line-height: 14pt;">Demikian Berita Acara ini dibuat dan
         ditandatangani bersama untuk dipergunakan sebagaimana mestinya.</p>
 
     <table style="width: 100%; border-collapse: collapse;">
         <tr>
             <td style="width: 35%;">
                 <div style="text-align: center;">
-                    <div style="margin: 10pt 0 50pt;">{{ $penandatangan1->jabatan }}</div>
+                    <div style="margin: 10pt 0 40pt;">{{ $penandatangan1->jabatan }}</div>
 
                     <h4 style="margin: 0 0 3pt 0; text-decoration: underline;">{{ $penandatangan1->nama }}</h4>
                     <h5 style="font-weight: normal; margin: 0;">NIP. {{ $penandatangan1->nip }}
@@ -151,7 +152,7 @@
             </td>
             <td style="width: 35%;">
                 <div style="text-align: center;">
-                    <div style="margin: 10pt 0 50pt;">{{ $penandatangan2->jabatan }}</div>
+                    <div style="margin: 10pt 0 40pt;">{{ $penandatangan2->jabatan }}</div>
 
                     <h4 style="margin: 0 0 3pt 0; text-decoration: underline;">{{ $penandatangan2->nama }}</h4>
                     <h5 style="font-weight: normal; margin: 0;">NIP. {{ $penandatangan2->nip }}</h5>
@@ -162,7 +163,7 @@
             <td colspan="3">
                 <div style="text-align: center;">
                     <span>Mengetahui,</span>
-                    <div style="margin: 5pt 0 50pt;">{{ $penandatangan3->jabatan }}</div>
+                    <div style="margin: 5pt 0 40pt;">{{ $penandatangan3->jabatan }}</div>
 
                     <h4 style="margin: 0 0 3pt 0; text-decoration: underline;">{{ $penandatangan3->nama }}</h4>
                     <h5 style="font-weight: normal; margin: 0;">NIP. {{ $penandatangan3->nip }}</h5>
